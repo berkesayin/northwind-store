11c9bca27cc68354f1721484e337a0a3db408ac7
Berke Sayın <sayinberke34@gmail.com>
2023-12-10 22:56:12 +0300

    Use Container, Row, Col from react-bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
+import { Container, Row, Col } from "react-bootstrap";
 import { CategoryList } from "./components/CategoryList";
 import { Navbar } from "./components/Navbar";
 import { ProductList } from "./components/ProductList";
 
 export const App = () => {
   return (
-    <div>
-      <Navbar />
-      <CategoryList />
-      <ProductList />
-    </div>
+    <>
+      <Container>
+        <Row>
+          <Navbar />
+        </Row>
+        <Row>
+          <Col xs={3}>
+            <CategoryList />
+          </Col>
+          <Col xs={9}>
+            <ProductList />
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
