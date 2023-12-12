e8c1f19a9545db505ca8549511466af54a65d9e1
Berke Sayın <sayinberke34@gmail.com>
2023-12-12 14:04:41 +0300

    Handle category change with states

--- src/components/CategoryList/index.tsx
+++ src/components/CategoryList/index.tsx
@@ -9,17 +9,26 @@ export const CategoryList = ({ info: { title } }: CategoryListProps) => {
   ];
 
   const [categories, setCategories] = useState<Category[]>(initialCategories);
+  const [currentCategory, setCurrentCategory] = useState<string | null>("");
+
+  const handleCategoryChange = (category: Category) => {
+    setCurrentCategory(category.categoryName);
+  };
 
   return (
     <div>
       <h3>{title}</h3>
       <ListGroup>
         {categories.map((category) => (
-          <ListGroup.Item key={category.categoryId}>
+          <ListGroup.Item
+            key={category.categoryId}
+            onClick={() => handleCategoryChange(category)}
+          >
             {category.categoryName}
           </ListGroup.Item>
         ))}
       </ListGroup>
+      <h4>{currentCategory}</h4>
     </div>
   );
 };
